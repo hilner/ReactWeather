1bf6c2e6fd3e0f85a8861dc96d3182adb1fe415f
hilner <hilner@alu.ufc.br>
2017-02-24 12:33:13 -0300

    heroku Nav appjsx

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -1,15 +1,48 @@
 var React = require('react');
 var {Link, IndexLink} = require('react-router');
 
-var Nav = (props) => {
-  return (
-    <div>
-      <h2>componente Nav</h2>
-      <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Ir para Weather</IndexLink>
-      <Link to="/about" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>About</Link>
-      <Link to="/examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Examples</Link>
-    </div>
-  )
-};
+var Nav = React.createClass({
+  onSearch: function(e){
+    e.preventDefault();
+    var location = this.refs.location.value;
+
+    alert('test');
+  },
+  render: function(){
+    return (
+      <div className="top-bar">
+        <div className="top-bar-left">
+        <ul className="menu">
+        <li className="menu-text">App Temperaturas React</li>
+      <li>
+        <IndexLink to="/" activeClassName="active"
+        activeStyle={{fontWeight: 'bold'}}>Temperaturas</IndexLink>
+      </li>
+      <li>
+        <Link to="/about" activeClassName="active"
+        activeStyle={{fontWeight: 'bold'}}>Sobre</Link>
+      </li>
+      <li>
+        <Link to="/examples" activeClassName="active"
+        activeStyle={{fontWeight: 'bold'}}>Exemplos</Link>
+      </li>
+      </ul>
+      </div>
+      <div className="top-bar-right">
+      <form onSubmit={this.onSearch}>
+        <ul className="menu">
+          <li>
+            <input type="search" placeholder="location"/>
+          </li>
+          <li>
+            <input className="button" type="button" value="obter temperatura"/>
+          </li>
+        </ul>
+      </form>
+      </div>
+      </div>
+    );
+  }
+});
 
 module.exports = Nav;
