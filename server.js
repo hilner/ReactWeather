68ff9f38f553394c4b27a49558e0d1ca8ac4f4d3
hilner <hilner@alu.ufc.br>
2017-02-24 09:36:24 -0300

    heroku2

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require('express');//express api access
 var app = express();
 const PORTA = process.env.PORT || 3000;
 app.use(function(req, res, next){
-  if(req.headers['-x-forwarded-proto'] === 'http'){
+  if(req.headers['x-forwarded-proto'] === 'http'){
     next();
   }else{
     res.redirect('http://'+req.hostname + req.url);
